dbaf29e60aaec1f713aadf659694634d3d03e337
Jasper Wallace <jasper@pointless.net>
2014-06-20 04:34:44 +0000

    make channels work

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -8,9 +8,9 @@ class NotifierHook < Redmine::Hook::Listener
     @author = @issue.author
     @channel = project_to_channel(@project.name)
     @assigned_message = @issue.assigned_to.nil? ? "No-one" : "#{@issue.assigned_to.firstname} #{@issue.assigned_to.lastname}"
-    speak "##{@channel} #{@author.firstname} #{@author.lastname} Created a new Issue:「#{@issue.subject}」"
-    speak "##{@channel} Status:「#{@issue.status.name}」. Assigned to:「#{@assigned_message}」. description:「#{truncate_words(@issue.description)}」"
-    speak "##{@channel} url: https://#{Setting.host_name}/issues/#{@issue.id}"
+    speak "#{@channel} #{@author.firstname} #{@author.lastname} Created a new Issue:「#{@issue.subject}」"
+    speak "#{@channel} Status:「#{@issue.status.name}」. Assigned to:「#{@assigned_message}」. description:「#{truncate_words(@issue.description)}」"
+    speak "#{@channel} url: https://#{Setting.host_name}/issues/#{@issue.id}"
   end
 
   def controller_issues_edit_before_save(context = { })
@@ -20,43 +20,42 @@ class NotifierHook < Redmine::Hook::Listener
     @editor = @journal.user
     @assigned_message = issue_assigned_changed?(@issue)
     @status_message = issue_status_changed?(@issue)
-    speak "##{@channel} #{@editor.firstname} #{@editor.lastname} edited:「#{@issue.subject}」"
-    speak "##{@channel} Status:「#{@status_message}」. Assigned to:「#{@assigned_message}」. description:「#{truncate_words(@journal.notes)}」"
-    speak "##{@channel} url: https://#{Setting.host_name}/issues/#{@issue.id}"
+    speak "#{@channel} #{@editor.firstname} #{@editor.lastname} edited:「#{@issue.subject}」"
+    speak "#{@channel} Status:「#{@status_message}」. Assigned to:「#{@assigned_message}」. description:「#{truncate_words(@journal.notes)}」"
+    speak "#{@channel} url: https://#{Setting.host_name}/issues/#{@issue.id}"
   end
 
   def controller_messages_new_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @author = @message.author
-    speak "##{@channel} #{@author.firstname} #{@author.lastname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
-    speak "##{@channel} url: https://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "#{@channel} #{@author.firstname} #{@author.lastname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
+    speak "#{@channel} url: https://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
 
   def controller_messages_reply_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @author = @message.author
-    speak "##{@channel} #{@author.firstname} #{@author.lastname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
-    speak "##{@channel} url: https://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "#{@channel} #{@author.firstname} #{@author.lastname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
+    speak "#{@channel} url: https://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
 
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
     @author = @page.content.author
-    speak "##{@channel} #{@author.firstname} #{@author.lastname} edited the wiki page「#{@page.pretty_title}」on #{@project.name}."
-    speak "##{@channel} url: https://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
+    speak "#{@channel} #{@author.firstname} #{@author.lastname} edited the wiki page「#{@page.pretty_title}」on #{@project.name}."
+    speak "#{@channel} url: https://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
   end
 
 private
 
   def project_to_channel(project)
-  	# XXX this should be in a config file somewhere.
-  	map = Hash.new("#emfcamp-test")
+    # XXX this should be in a config file somewhere.
+    map = Hash.new("#emfcamp-test")
     map["Noc"] = "#emfcamp-noc"
-    map["Test"] = "#emfcamp-test"
-    map["sysadmin"] = "#emfcamp-test,@jasperw"
+    map["Sysadmin"] = "#emfcamp-test,@jasperw"
     map[project]
   end
 
