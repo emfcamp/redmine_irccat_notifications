20c8a9ccbc5a37105001ffdeef9c8e20111fc5ea
Jasper Wallace <jasper@pointless.net>
2014-06-20 04:21:59 +0000

    translate, make netcat quit, try to send to the right channels

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@
 require 'redmine'
 require_dependency 'notifier_hook'
 
-Redmine::Plugin.register :redmine_campfire_notifications do
+Redmine::Plugin.register :redmine_irccat_notifications do
   name 'Redmine IRC Notifications plugin'
   author 'xdite'
   description 'A plugin to display issue modifications to a Irc Channel'
--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -6,10 +6,11 @@ class NotifierHook < Redmine::Hook::Listener
     @project = context[:project]
     @issue = context[:issue]
     @author = @issue.author
-    @assigned_message = @issue.assigned_to.nil? ? "無" : "#{@issue.assigned_to.lastname} #{@issue.assigned_to.firstname}"
-    speak "#{@author.lastname} #{@author.firstname} 建立主旨:「#{@issue.subject}」"
-    speak "狀態:「#{@issue.status.name}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@issue.description)}」"
-    speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
+    @channel = project_to_channel(@project.name)
+    @assigned_message = @issue.assigned_to.nil? ? "No-one" : "#{@issue.assigned_to.firstname} #{@issue.assigned_to.lastname}"
+    speak "##{@channel} #{@author.firstname} #{@author.lastname} Created a new Issue:「#{@issue.subject}」"
+    speak "##{@channel} Status:「#{@issue.status.name}」. Assigned to:「#{@assigned_message}」. description:「#{truncate_words(@issue.description)}」"
+    speak "##{@channel} url: https://#{Setting.host_name}/issues/#{@issue.id}"
   end
 
   def controller_issues_edit_before_save(context = { })
@@ -19,39 +20,48 @@ class NotifierHook < Redmine::Hook::Listener
     @editor = @journal.user
     @assigned_message = issue_assigned_changed?(@issue)
     @status_message = issue_status_changed?(@issue)
-    speak "#{@editor.lastname} #{@editor.firstname} 編輯主旨:「#{@issue.subject}」"
-    speak "狀態:「#{@status_message}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@journal.notes)}」"
-    speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
+    speak "##{@channel} #{@editor.firstname} #{@editor.lastname} edited:「#{@issue.subject}」"
+    speak "##{@channel} Status:「#{@status_message}」. Assigned to:「#{@assigned_message}」. description:「#{truncate_words(@journal.notes)}」"
+    speak "##{@channel} url: https://#{Setting.host_name}/issues/#{@issue.id}"
   end
 
   def controller_messages_new_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @author = @message.author
-    speak "#{@author.lastname} #{@author.firstname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
-    speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "##{@channel} #{@author.firstname} #{@author.lastname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
+    speak "##{@channel} url: https://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
 
   def controller_messages_reply_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
     @author = @message.author
-    speak "#{@author.lastname} #{@author.firstname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
-    speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "##{@channel} #{@author.firstname} #{@author.lastname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
+    speak "##{@channel} url: https://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
 
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
     @author = @page.content.author
-    speak "#{@author.lastname} #{@author.firstname} edited the wiki「#{@page.pretty_title}」on #{@project.name}."
-    speak "網址: http://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
+    speak "##{@channel} #{@author.firstname} #{@author.lastname} edited the wiki page「#{@page.pretty_title}」on #{@project.name}."
+    speak "##{@channel} url: https://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
   end
 
 private
 
+  def project_to_channel(project)
+  	# XXX this should be in a config file somewhere.
+  	map = Hash.new("#emfcamp-test")
+    map["Noc"] = "#emfcamp-noc"
+    map["Test"] = "#emfcamp-test"
+    map["sysadmin"] = "#emfcamp-test,@jasperw"
+    map[project]
+  end
+
   def speak(message)
-    system("echo -n '#{message.delete("[\"\']")}' | nc localhost 5678")
+    system("echo -n '#{message.delete("[\"\']")}' | nc -q0 localhost 12345")
   end
 
   def truncate_words(text, length = 20, end_string = '…')
@@ -63,7 +73,7 @@ private
   def issue_status_changed?(issue)
     if issue.status_id_changed?
       old_status = IssueStatus.find(issue.status_id_was)
-      "從 #{old_status.name} 變更為 #{issue.status.name}"
+      "changed from #{old_status.name} to #{issue.status.name}"
     else
       "#{issue.status.name}"
     end
@@ -72,11 +82,11 @@ private
   def issue_assigned_changed?(issue)
     if issue.assigned_to_id_changed?
       old_assigned_to = User.find(issue.assigned_to_id_was) rescue nil
-      old_assigned = old_assigned_to.nil? ? "無" : "#{old_assigned_to.lastname} #{old_assigned_to.firstname}"
-      new_assigned = issue.assigned_to.nil? ? "無" : "#{issue.assigned_to.lastname} #{issue.assigned_to.firstname}"
-      "從 #{old_assigned} 變更為 #{new_assigned}"
+      old_assigned = old_assigned_to.nil? ? "no-one" : "#{old_assigned_to.firstname} #{old_assigned_to.lastname}"
+      new_assigned = issue.assigned_to.nil? ? "no-one" : "#{issue.assigned_to.firstname} #{issue.assigned_to.lastname}"
+      "from #{old_assigned} to #{new_assigned}"
     else
-      issue.assigned_to.nil? ? "無" : "#{issue.assigned_to.lastname} #{issue.assigned_to.firstname}"
+      issue.assigned_to.nil? ? "no-one" : "#{issue.assigned_to.firstname} #{issue.assigned_to.lastname}"
     end
   end
 
